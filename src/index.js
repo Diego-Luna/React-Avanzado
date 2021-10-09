7191706b132fd60074748e2f1a0fd61e238e0fd8
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-09 09:39:23 -0500

    Instalación de React y Babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("empesamos 🎉");
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(<h1>Seguimos con el curso avanzado!</h1>, document.getElementById('app'))
