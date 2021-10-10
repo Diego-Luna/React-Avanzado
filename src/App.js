14788750277827aeded878b96d6047db9cd06a51
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-10 06:58:43 -0500

    Creando nuestro primer componente: Category

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Category } from './components/category';
+
+function App() {
+  return (
+    <div>
+      <Category />
+      <h1>Seguimos con el curso avanzado!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/category/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/prefer-default-export */
+
+import React from 'react';
+import { Anchor, Image } from './styles';
+
+const DEFAULT_IMGE = 'https://i.imgur.com/dJa0Hpl.jpg';
+
+export const Category = ({ cover = DEFAULT_IMGE, path, emoji = '?' }) => (
+  <Anchor href={path}>
+    <Image src={cover} alt='imagen de categoria' />
+    {emoji}
+  </Anchor>
+);
