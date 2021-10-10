aece97a8192eed22045e421ea3dd204b7637d292
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-10 08:35:52 -0500

    Creando animaciones con keyframes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Logo } from './components/Logo';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotocars } from './components/ListOfPhotoCards';
-import { GlobalStyle } from './Globalstyles';
+import { GlobalStyle } from './styles/Globalstyles';
 
 function App() {
   return (
