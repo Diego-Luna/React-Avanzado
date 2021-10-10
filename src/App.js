1c3bd0bd829f399c364293cdfba76ff294f72ef3
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-10 08:22:10 -0500

    SVGR: de SVG a componente de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Logo } from './components/Logo';
 import { ListOfCategories } from './components/ListOfCategories';
 import { ListOfPhotocars } from './components/ListOfPhotoCards';
 import { GlobalStyle } from './Globalstyles';
@@ -7,6 +8,7 @@ import { GlobalStyle } from './Globalstyles';
 function App() {
   return (
     <>
+      <Logo />
       <GlobalStyle />
       <ListOfCategories />
       <ListOfPhotocars />
