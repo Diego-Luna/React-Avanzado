38db4035166a9128c7847a676174b332861b41c8
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-11 20:09:28 -0500

    useCategoriesData

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,11 +4,12 @@ import React, { useState, useEffect } from 'react';
 import { Category } from '../Category';
 import { List, Item } from './styles';
 
-export const ListOfCategories = () => {
+function useCategoriesData() {
   const [categories, setCategories] = useState([]);
-  const [showFixed, setShowFixed] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     window
       .fetch(
         'https://petgram-server-diego-luna-v2-diego-luna.vercel.app/categories'
@@ -16,8 +17,17 @@ export const ListOfCategories = () => {
       .then((res) => res.json())
       .then((response) => {
         setCategories(response);
+        setLoading(false);
       });
   }, []);
+
+  return { categories, loading };
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData();
+
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
@@ -31,12 +41,18 @@ export const ListOfCategories = () => {
   }, [showFixed]);
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
-      {categories.map((category) => (
-        <Item key={category.id}>
-          <Category {...category} />
+    <List fixed={fixed}>
+      {loading ? (
+        <Item key={loading}>
+          <Category />
         </Item>
-      ))}
+      ) : (
+        categories.map((category) => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      )}
     </List>
   );
 
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { bounceDown } from '../../styles/animation';
 
 export const List = styled.ul`
@@ -10,20 +10,23 @@ export const List = styled.ul`
     display: none;
   }
 
-  &.fixed {
-    background-color: #fff;
-    border-radius: 60px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
-    left: 0% auto;
-    max-width: 400px;
-    padding: 5px;
-    position: fixed;
-    right: 0;
-    top: -20px;
-    transform: scale(0.5);
-    z-index: 1;
-    ${bounceDown()}
-  }
+  ${(props) =>
+    props.fixed
+      ? css`
+          background-color: #fff;
+          border-radius: 60px;
+          box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+          left: 0% auto;
+          max-width: 400px;
+          padding: 5px;
+          position: fixed;
+          right: 0;
+          top: -20px;
+          transform: scale(0.5);
+          z-index: 1;
+          ${bounceDown()}
+        `
+      : ''}
 `;
 
 export const Item = styled.li`
