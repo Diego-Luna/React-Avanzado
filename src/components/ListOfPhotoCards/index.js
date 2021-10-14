6baa2ecacdd6ec0ec0686e13fc46b5e57c599e87
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-14 07:39:19 -0500

    ¿Qué es GraphQL y React Apollo? Inicializando React Apollo Client y primer HoC

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,58 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { PhotoCard } from '../PhotoCard';
 
-export const ListOfPhotocars = () => (
-  <ul>
-    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => (
-      <PhotoCard key={id} id={id} />
-    ))}
-  </ul>
-);
+const whitPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
+export const ListOfPhotocars = () => {
+  const { loading, error, data } = useQuery(whitPhotos);
+
+  if (error) {
+    return <h2>Internal Server Error</h2>;
+  }
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <ul>
+      {data.photos.map((photoCard, id) => (
+        <PhotoCard key={id} {...photoCard} />
+        // <PhotoCard key={id} id={id} {...photoCard} />
+      ))}
+    </ul>
+  );
+};
+
+// export const ListOfPhotocars = () => {
+//   const { loading, error, data } = useQuery(whitPhotos);
+
+//   if (error) {
+//     return <h2>Internal Server Error</h2>;
+//   }
+//   if (loading) {
+//     return <h2>Loading...</h2>;
+//   }
+
+//   return (
+//     <ul>
+//       {data.photos.map((photo) => (
+//         <PhotoCard key={photo.id} {...photo} />
+//       ))}
+//     </ul>
+//   );
+// };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// uri: 'https://petgram-server-jrmfsd-okxluew9o.now.sh/graphql'
+const client = new ApolloClient({
+  uri: 'https://petgram-server-diego-luna-v2-pjzqjew1c-diego-luna.vercel.app/graphql',
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+);
