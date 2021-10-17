2859dcccd5c715ba34c9699caaac30c026f9e2db
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-17 07:34:40 -0500

    Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ import { GlobalStyle } from './styles/Globalstyles';
 
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
 
 import { NavBar } from './components/NavBar';
+
+const UserLogged = ({ children }) => children({ isAuth: false });
 
 function App() {
   return (
@@ -26,6 +31,22 @@ function App() {
         <Route exact path="/detail/:detailId">
           <Detail />
         </Route>
+
+        <UserLogged>
+          {({ isAuth }) =>
+            isAuth ? (
+              <>
+                <Route exact path="/favs" component={Favs} />
+                <Route exact path="/user" component={User} />
+              </>
+            ) : (
+              <>
+                <Route exact path="/favs" component={NotRegisteredUser} />
+                <Route exact path="/user" component={NotRegisteredUser} />
+              </>
+            )
+          }
+        </UserLogged>
       </Switch>
 
       <NavBar />
