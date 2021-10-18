6f72b120e90c5d55145db82d84277d09af6ca129
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-18 07:55:43 -0500

    Mutaciones para registro

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,17 +1,41 @@
+/* eslint-disable func-names */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
 import Context from '../Context';
 import { UserForm } from '../components/UserForm';
+import { useRegisterMutation } from '../container/RegisterMutation';
 
-export const NotRegisteredUser = () => (
-  <Context.Consumer>
-    {function ({ activateAuth }) {
-      return (
-        <>
-          <UserForm title="Registrase" onSubmit={activateAuth} />
-          <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
-        </>
-      );
-    }}
-  </Context.Consumer>
-);
+export const NotRegisteredUser = () => {
+  const { registerMutation } = useRegisterMutation();
+
+  return (
+    <Context.Consumer>
+      {({ activateAuth }) => {
+        const onSubmit = ({ email, password }) => {
+          const input = { email, password };
+          const variables = { input };
+          registerMutation({ variables }).then(activateAuth);
+        };
+        return (
+          <>
+            <UserForm onSubmit={onSubmit} title="Registrarse" />
+            <UserForm onSubmit={activateAuth} title="Iniciar Sesion" />
+          </>
+        );
+      }}
+    </Context.Consumer>
+  );
+
+  // return (
+  //   <Context.Consumer>
+  //     {function ({ activateAuth }) {
+  //       return (
+  //         <>
+  //           <UserForm title="Registrase" onSubmit={activateAuth} />
+  //           <UserForm title="Iniciar sesión" onSubmit={activateAuth} />
+  //         </>
+  //       );
+  //     }}
+  //   </Context.Consumer>
+  // );
+};
