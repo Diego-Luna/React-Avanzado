751e89e846e0b3faac2457e4555a4709579b370b
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-21 10:37:10 -0500

    Agregando Middleware en apollo

--- src/index.js
+++ src/index.js
@@ -1,44 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+  from,
+  HttpLink,
+} from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
 
 import Contex from './Context';
 
 import App from './App';
 
-// const client = new ApolloClient({
+// const httpLink = createHttpLink({
 //   uri: 'https://petgram-server-diego-luna-v2-pjzqjew1c-diego-luna.vercel.app/graphql',
-//   cache: new InMemoryCache(),
-//   request: (operation) => {
-//     const token = window.sessionStorage.getItem('token');
-//     const authorization = token? `Bearer ${token}`: "";
-//     operation.setContext({
-//       headers: {
-//         authorization
-//       }
-//     })
-//   },
 // });
 
-const httpLink = createHttpLink({
-  uri: 'https://petgram-server-diego-luna-v2-pjzqjew1c-diego-luna.vercel.app/graphql'
-})
+// const authLink = setContext((_, { headers }) => {
+//   const token = window.sessionStorage.getItem('token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
-const authLink = setContext((_, { headers }) => {
-  const token = window.sessionStorage.getItem('token')
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem('token');
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
   }
-})
+  return forward(operation);
+});
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === 'invalid_token') {
+    window.sessionStorage.removeItem('token');
+    window.location = '/user';
+  }
+});
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-})
+  cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: 'https://petgram-server-diego-luna-v2-pjzqjew1c-diego-luna.vercel.app/graphql',
+    }),
+  ]),
+});
 
 ReactDOM.render(
   <Contex.Provider>
