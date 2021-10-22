f7923b287cb222a605c12725daa8936dc0eae986
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-22 08:36:11 -0500

    Mostrar favoritos y solucionar fetch policy 1/2

---
+++ src/hooks/useGetFavorites.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/prefer-default-export */
+import { useQuery, gql } from '@apollo/client';
+
+const GET_FAVS = gql`
+  query getFavs {
+    favs {
+      id
+      categoryId
+      src
+      likes
+      userId
+    }
+  }
+`;
+export const useGetFavorites = (id) => {
+  const { loading, data, error } = useQuery(GET_FAVS, {
+    fetchPolicy: 'network-only',
+  });
+  return { loading, data, error };
+};
--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,4 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
+import { PhotoCard } from '../components/PhotoCard';
 
-export const Favs = () => <h1>Favs</h1>;
+import { useGetFavorites } from '../hooks/useGetFavorites';
+
+export const Favs = () => {
+  const { loading, data, error } = useGetFavorites();
+
+  console.log("- data ", data);
+
+  if(error){
+    return <h1>Error</h1>
+  }
+
+  if(loading){
+    return <h1>Loading</h1>
+  }
+
+  return <>
+  {data.favs.map((photo) => (
+        <PhotoCard key={photo.id} liked {...photo} />
+        // <PhotoCard key={id} id={id} {...photoCard} />
+      ))}
+  </>;
+};
