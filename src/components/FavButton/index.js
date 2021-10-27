9233ba2310159e0e4116e55b84b471825c2521dc
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-27 12:17:16 -0500

    Usando PropTypes para validar las props  1/2

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,6 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/require-default-props */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
+import PropTypes from 'prop-types';
 import { Button } from './styles';
 
 export const FavButton = ({ liked, likes, onClick }) => {
@@ -11,4 +14,10 @@ export const FavButton = ({ liked, likes, onClick }) => {
       <Icon size="32px" /> {likes} likes!
     </Button>
   );
+};
+
+FavButton.propTypes = {
+  liked: PropTypes.bool,
+  likes: PropTypes.number,
+  onClick: PropTypes.func,
 };
