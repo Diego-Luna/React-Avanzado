116d51b7b8c3729744233f140dc287e4fb99f29f
Dieg Luna <diegofcolunalopez@gmail.com>
2021-10-29 19:46:47 -0500

    Usando PropTypes para validar las props  2/4

--- src/components/ListOdFavs/index.js
+++ src/components/ListOdFavs/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/require-default-props */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link, Grid, Image } from './styles';
 
 export const ListOfFavs = ({ favs = [] }) => (
@@ -11,3 +14,12 @@ export const ListOfFavs = ({ favs = [] }) => (
     ))}
   </Grid>
 );
+
+ListOfFavs.propTypes = {
+  favs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    })
+  ),
+};
